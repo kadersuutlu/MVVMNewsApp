9a3450f676d3e7c2495d5e0b8896bc302396116f
kadersutlu <sutlukader@gmail.com>
2023-11-27 19:54:23 +0300

    Api_Key made private

--- app/src/main/java/com/kader/newsapp/util/Constants.kt
+++ app/src/main/java/com/kader/newsapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.kader.newsapp.util
 
 class Constants {
     companion object {
-        const val API_KEY = "79761718f8594ad698d08450751c6c2f"
+        const val API_KEY = "YOUR_API_KEY"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SİZE = 20
